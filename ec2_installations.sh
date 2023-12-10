4fd2ad3f7593229f9fc08acaa1cb8c8ec08ddeb9
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-12-10 22:30:11 +0200

    Update ec2_installations.sh

--- ec2_installations.sh
+++ ec2_installations.sh
@@ -1,12 +1,23 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install micro -y
+#!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+
+# 1 GB swap memory
+sudo swapon -s
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
+sudo swapon -s
+
+# intstall docker & docker-compose
 sudo apt install docker.io -y
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# grant non-root users access to Docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# intstall micro (terminal text editor)
+sudo apt install micro -y

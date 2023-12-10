572325166cbeed8454f19eb4c9d8818be7d05a4f
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-12-10 21:37:53 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,10 @@
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt install micro -y
+
 sudo apt install docker.io -y
-sudo apt install docker-compose -y
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # grant non-root users access to Docker
 sudo groupadd docker

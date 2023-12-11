9cd38612e702ac3b643b82d52598d8a9ff65fcf3
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-12-11 10:35:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 mkdir developer && cd developer
-git clone https://github.com/omarhosny206/ubuntu.git setup
+git clone https://omarhosny206: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@github.com/omarhosny206/ubuntu.git setup
 cd setup
 sh ./ec2_setup.sh

edadb371a810b30fea095a0e0f33abdadc157336
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-12-11 09:57:35 +0200

    Update ec2_setup.sh

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -11,19 +11,17 @@ sudo swapon /swapfile
 echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
 sudo swapon -s
 
-# Install Docker
+# install Docker
 sudo apt install docker.io -y
-sudo groupadd docker
-sudo usermod -aG docker $USER
 
-# Install Docker Compose
+# install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Install micro (terminal text editor)
+# install micro (terminal text editor)
 sudo apt install micro -y
 
-# Create 'docker' group, add user, and start a new session
+# create 'docker' group, add user, and start a new session
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker

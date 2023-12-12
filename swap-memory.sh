1652de1058b84b4af2eae2f227b8bfb9c16e58c4
Omar Hosny Keshk <omarhosny102@gmail.com>
2023-12-12 20:05:00 +0200

    Update and rename swap-memory-commands.sh to swap-memory.sh

--- swap-memory.sh
+++ swap-memory.sh
@@ -1,8 +1,8 @@
+# create 1 GB of swap memory
 sudo swapon -s
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo micro /etc/fstab
-# add this line --> /swapfile swap swap defaults 0 0
+echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
 sudo swapon -s

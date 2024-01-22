6580a0ef4b717406b55ae8dc174060d09de1f165
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-22 20:33:18 +0200

    Add setup-without-swap-memory.sh

---
+++ setup-without-swap-memory.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+# install Docker
+sudo apt install docker.io -y
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# install micro (terminal text editor)
+sudo apt install micro -y
+
+# clean developer directory
+cd .. && rm -rf aws-ubuntu-setup
+
+# install AWS CLI V2
+cd /
+sudo apt install unzip -y
+sudo curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
+
+# create 'docker' group to ubuntu user (non-root user)
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker

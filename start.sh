4799e1bb9d6274538cbe39f5aa5f0599ce508f63
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-22 02:40:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 mkdir developer && cd developer
-git clone https://github.com/omarhosny206/ubuntu-setup.git
+git clone https://github.com/omarhosny206/aws-ubuntu-setup.git
 cd ubuntu-setup
 sh ./setup.sh

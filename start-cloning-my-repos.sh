444187fbcfdac8e43aa563f3a603f6804a1dd26d
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-26 02:49:39 +0200

    Add start-cloning-my-repos.sh

---
+++ start-cloning-my-repos.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/omarhosny206/aws-ubuntu-setup.git
+sh ./aws-ubuntu-setup/clone-my-repos.sh docker docker-compose tribe online-ordering-service apple-pay design-patterns SOLID-Principles leetcode-problems

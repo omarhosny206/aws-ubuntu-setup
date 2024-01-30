cdac71d68479f204b3b066cef8d572b00d4e768c
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-30 12:16:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ sudo apt install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# install micro (terminal text editor)
+# install micro (terminal-based text editor)
 sudo apt install micro -y
 
 # clean developer directory
